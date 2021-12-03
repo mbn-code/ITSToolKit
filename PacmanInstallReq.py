bc2727df7f60def8e4aeb3799c863c6aace6dd36
CollinEdward <manorengaard@icloud.com>
2021-12-03 22:29:30 +0100

    Apt and Pacman commands added for auto install packages.

---
+++ PacmanInstallReq.py
@@ -0,0 +1,35 @@
+import os
+
+def install():
+    
+    # Install the pip3 requirements
+    os.system("pip3 install os")
+    os.system("pip3 install time")
+    os.system("pip3 install platform")
+    os.system("pip3 install calendar")
+    os.system("pip3 install hashlib")
+    
+    # Now install Apt requirements
+    os.system("sudo pacman -S neofetch")
+    
+def uninstall():
+    
+    # remove the pip3 requirements
+    os.system("pip3 uninstall os")
+    os.system("pip3 uninstall time")
+    os.system("pip3 uninstall platform")
+    os.system("pip3 uninstall calendar")
+    os.system("pip3 uninstall hashlib")
+    
+    # Now uninstall Apt requirements
+    os.system("sudo apt remove neofetch")
+    
+
+if __name__ == "__main__":
+    print("Pacman installer / uninstaller")
+    x_input = input("Do you want to install or uninstall?(I/UI): ")
+
+    if x_input.lower == "i":
+        install()
+    elif x_input.lower() == "ui":
+        uninstall()
